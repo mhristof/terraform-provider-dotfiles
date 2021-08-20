4509ae6b7e3a01e7e4092cbdd8f2fc7e55fca538
Mike Christof <mhristof@gmail.com>
2021-08-20 15:03:17 +0100

    add tests for links

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/mhristof/terraform-provider-dotfiles
 
 go 1.16
 
-require github.com/hashicorp/terraform-plugin-sdk v1.17.2
+require (
+	github.com/MakeNowJust/heredoc v1.0.0 // indirect
+	github.com/gruntwork-io/terratest v0.37.7
+	github.com/hashicorp/terraform-plugin-sdk v1.17.2
+	github.com/otiai10/copy v1.6.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
---
+++ resource_link_test.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func createFs(t *testing.T, files map[string]string) (string, func()) {
+	dir, err := ioutil.TempDir("", "prefix")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for name, content := range files {
+		err = ioutil.WriteFile(filepath.Join(dir, name), []byte(content), 0644)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+	}
+
+	return dir, func() {
+		os.RemoveAll(dir)
+	}
+}
+
+func TestLink(t *testing.T) {
+	var cases = []struct {
+		name     string
+		fs       map[string]string
+		validate func(string) error
+	}{
+		{
+			name: "simple link creation",
+			fs: map[string]string{
+				"zshrc": heredoc.Doc(`
+					this file is intentionally left blank
+				`),
+				"main.tf": heredoc.Doc(`
+					provider "dotfiles" {}
+
+					resource "dotfiles_link" "foo" {
+					  dest         = ".zshrc"
+					  source       = "zshrc"
+					}
+				`),
+			},
+			validate: func(dir string) error {
+				if _, err := os.Stat(filepath.Join(dir, ".zshrc")); os.IsNotExist(err) {
+					return err
+				}
+
+				return nil
+			},
+		},
+	}
+
+	for _, test := range cases {
+		dir, cleanup := createFs(t, test.fs)
+		defer cleanup()
+
+		terraformD, err := filepath.Abs("./terraform.d")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		providerTf, err := filepath.Abs("./providers.tf")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		os.Symlink(terraformD, fmt.Sprintf("%s/terraform.d", dir))
+		os.Symlink(providerTf, fmt.Sprintf("%s/providers.tf", dir))
+
+		fmt.Println(fmt.Sprintf("dir: %+v", dir))
+
+		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+			TerraformDir: dir,
+		})
+
+		defer terraform.Destroy(t, terraformOptions)
+		terraform.InitAndApply(t, terraformOptions)
+
+		assert.Nil(t, test.validate(dir), test.name)
+	}
+}
